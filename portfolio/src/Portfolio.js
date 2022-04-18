eb0b19e403854894994e24e7848fcc7300b9c2d9
Queendom <alice.alizarin13@gmail.com>
2022-04-17 21:45:06 -0400

    Updated Portoflio to use carosuel
    Replaced thumbnail with react-responsive-carosel components.

--- portfolio/src/Portfolio.js
+++ portfolio/src/Portfolio.js
@@ -1,44 +1,43 @@
 import React from "react"
 import Thumbnail from './Thumbnail.js'; // Import the Thumbnail component
 import './App.css';
-import TheCryptogropherLarge  from './images/TheCryptogropherLarge.jpg';
-import TheCryptogropherMedium from './images/TheCryptogropherMedium.jpg';
-import TheCryptogropherSmall from './images/TheCryptogropherSmall.jpg';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 import JJBAWikiSpot from './images/JJBAWikiSpotLarge.jpg';
 import WestPalmBeach from './images/WestPalmBeachLarge.jpg';
+import TheCryptogropher from './images/TheCryptogropher.PNG';
  
 function Portfolio(props) {
   return (
     <div>
       <h1>Portfolio</h1>
-      <Thumbnail 
-        link="https://wcet3.waketech.edu/dpablo/WEB250mos/wordpress/"
-        image_small={TheCryptogropherSmall}
-        image_medium={TheCryptogropherMedium}
-        image_large={TheCryptogropherLarge}
-        title="The Cryptogropher"
-        category=""
-      />
-    <p class="CryptoTagline">A cryptocurrency website that was created for a class mid-term.</p>
-
-      <Thumbnail
-        link="https://wcet3.waketech.edu/dpablo/Web125/GrandFinale/index.html"
-        image={JJBAWikiSpot}
-        title="JJBA WikiSpot"
-        category=""
-      />
-
-      <p>This was a Jojo's Bizarre Adventure fanmade website that I created for a class final. 
+      <Carousel>
+        <div>
+          <a href="https://wcet3.waketech.edu/dpablo/WEB250mos/wordpress/" target="_blank">
+            <div>
+              <img src={TheCryptogropher} />
+            </div>
+          </a>
+            <p className="legend">A cryptocurrency website that was created for a class mid-term.</p>
+        </div>
+        <div>
+          <a href="https://wcet3.waketech.edu/dpablo/Web125/GrandFinale/index.html" target="_blank">
+            <div>
+              <img src={JJBAWikiSpot} />
+            </div>
+          </a>
+            <p className="legend">This was a Jojo's Bizarre Adventure fanmade website that I created for a class final. 
         This was designed to be for mobile devices.</p>
-
-        <Thumbnail
-        link="https://wcet3.waketech.edu/dpablo/WEB250eos/wordpress/news/"
-        image={WestPalmBeach}
-        title="West Palm Beach"
-        category=""
-      />
-
-      <p>As a class final, I created a tourist website for West Palm Beach, Florida.</p>
+        </div>
+        <div>
+          <a href="https://wcet3.waketech.edu/dpablo/WEB250eos/wordpress/news/" target="_blank">
+            <div>
+              <img src={WestPalmBeach} />
+            </div>
+          </a>
+            <p className="legend">As a class final, I created a tourist website for West Palm Beach, Florida.</p>
+        </div>
+      </Carousel>
     </div>
   )
 }
